7c210a8fac48e067d6e1d0e55eeaf405c1028553
Guilherme Alencar <guilhermesalencar@gmail.com>
2018-12-09 12:21:14 +0000

    Initial Commit

---
+++ stocker-master/app/controllers/home_controller.rb
@@ -0,0 +1,17 @@
+class HomeController < ApplicationController
+  def index
+    if params[:id] == ""
+      @nothing = "Hey, you forgot to enter a symbol"
+    elsif
+    
+      if params[:id]
+        @stock = StockQuote::Stock.quote(params[:id])
+      end
+    end
+  end
+  
+  
+  def about
+  end
+  
+end
